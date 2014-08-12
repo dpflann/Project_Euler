d4a93aa17ec28de635ee13f1058fe2272fde6fbb
Daniel Flannery <d.p.flannery@gmail.com>
2014-08-12 08:24:33 -0400

    Project Euler Problem 20: Sum digits of 100

---
+++ pe20/pe20.py
@@ -0,0 +1,23 @@
+# Find the sum of the digits in the number 100!
+import math
+
+def sumDigitsInFactorial(n):
+  number = math.factorial(n)
+  numberAsString= str(number)
+  sumTotal = 0
+  for d in numberAsString:
+    sumTotal = sumTotal + int(d)
+  return sumTotal
+
+def sDiF(n):
+  return reduce(lambda x, y: int(x) + int(y), str(math.factorial(n)), 0 )
+
+
+
+#####################
+def solve():
+  print("The sum of the digits in the number 100! is %d." % sumDigitsInFactorial( 100 ))
+  print("The sum of the digits in the number 100! is %d." % sDiF( 100 ))
+
+
+solve()
