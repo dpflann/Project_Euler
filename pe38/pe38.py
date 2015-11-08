7529f93c644af24d7dde296dbdd3f69eae9c3a3f
dpflann <d.p.flannery@gmail.com>
2015-11-07 21:03:18 -0500

    style?

--- pe38/pe38.py
+++ pe38/pe38.py
@@ -20,4 +20,3 @@ LIMIT=100000
 
 def solve():
   print("The largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2,3,..,n) where n > 1 is: %d" % find_concatenated_pandigital(LIMIT=LIMIT))
-
