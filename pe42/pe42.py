0a2e2e46ead4d71529c1565c6686d0804f32fd1a
dpflann <d.p.flannery@gmail.com>
2015-11-07 21:03:53 -0500

    Project Euler Problem 42 Solution: Triangular Number Words

---
+++ pe42/pe42.py
@@ -0,0 +1,20 @@
+# How many words in the accompnying txt file have a sum of characters (value based upon alphabetical position) that is a triangle number?
+
+with open('p042_words.txt', 'r') as words_txt:
+  words = [w.replace('"', '') for w in words_txt.read().split(',')]
+
+def construct_n_triangle_numbers(n):
+  return [int(.5 * i * (i + 1)) for i in range(1, n +1)]
+
+def convert_to_position(c):
+  return ord(c) - ord('A') + 1
+
+def calculate_value(word):
+  return sum(map(convert_to_position, word))
+
+def solve():
+  triangular_numbers = construct_n_triangle_numbers(100)
+  triangular_words = [w for w in words if calculate_value(w) in triangular_numbers]
+  print('The number of "triangular words" is: %d' % len(triangular_words))
+
+solve()
