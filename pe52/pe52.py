14d2fb08da215928cce6857b68c1e1d75f60ef64
dpflann <d.p.flannery@gmail.com>
2015-12-08 20:50:11 -0500

    Update pe52 solution

--- pe52/pe52.py
+++ pe52/pe52.py
@@ -1,8 +1,8 @@
 def is_permuted_multiple(n, limit=7):
-  str_n = sorted(str(n))
+  str_n = set(str(n))
   for i in range(2, limit):
     n_by_i = i * n
-    if sorted(str(n_by_i)) != str_n:
+    if set(str(n_by_i)) != str_n:
       return False
   return True
 
@@ -11,7 +11,7 @@ def solve():
   while not is_permuted_multiple(i):
     i += 1
   # assumes it's possible to solve this... :)
-  print("The smallest positive integer x such that 2x, 3x, 4x, 5x, and 6x contain the same digits is %d" % i)
+  print("The smallest positive integer x such that 2x, 3x, 4x, 5x, and 6x contains the same digits is %d" % i)
 
 if __name__ == '__main__':
   solve()
