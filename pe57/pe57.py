46491996e88cd27e66d3ce35f9f2ae9a56d9786e
dpflann <d.p.flannery@gmail.com>
2015-12-14 22:53:26 -0500

    Alternative solution which only keeps previous expansions and returns only count

--- pe57/pe57.py
+++ pe57/pe57.py
@@ -10,6 +10,18 @@ def find_top_heavy_expansions(number_of_expansions=1000):
       count += 1
   return expansions, count
 
+def find_top_heavy_expansions_count(number_of_expansions=1000):
+  count = 0
+  previous_numerator = 3
+  previous_denominator = 2
+  for _ in range(number_of_expansions - 1):  # account for seeding with first expansion
+    next_numerator = previous_numerator + 2 * previous_denominator
+    next_denominator = previous_numerator + previous_denominator
+    if len(str(next_numerator)) > len(str(next_denominator)):
+      count += 1
+    previous_numerator, previous_denominator = next_numerator, next_denominator
+  return count
+
 def solve():
   expansions, count = find_top_heavy_expansions()
   print('In the first 1000 expansions of 2^(1/2) as a continued fraction, the number of fractions with a numerator longer than the denominator is %d' % count)
