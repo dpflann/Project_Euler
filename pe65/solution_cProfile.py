399a4d0c5bb916b3cb7f9d199d72fe6c836df0a6
dpflann <d.p.flannery@gmail.com>
2015-12-15 00:33:49 -0500

    Problem 65 cProfile stats

---
+++ pe65/solution_cProfile.py
@@ -0,0 +1,18 @@
+The sum of the digits in the numerator of the 100th convergent of e is: 272
+         42 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 pe65.py:1(<module>)
+        1    0.000    0.000    0.000    0.000 pe65.py:1(convergent)
+        1    0.000    0.000    0.000    0.000 pe65.py:14(expand_e_to_nth_convergent)
+        1    0.000    0.000    0.000    0.000 pe65.py:24(sum_of_numerator_digits_in_expansion)
+        1    0.000    0.000    0.000    0.000 pe65.py:28(solve)
+        1    0.000    0.000    0.000    0.000 {len}
+        1    0.000    0.000    0.000    0.000 {map}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+       33    0.000    0.000    0.000    0.000 {method 'extend' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {sum}
+
+
